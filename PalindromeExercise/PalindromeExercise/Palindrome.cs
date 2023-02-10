2cbc2b5c497a47e7fec6a0cba72e39e8e67ec37d
danielocampo226 <sierradanny562@gmail.com>
2023-02-09 21:50:58 -0500

    Palindrome

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+       public bool IsAPalidrome(string word)
+        {
+            //reverse the word    //racecar Length --> 7 --> 6
+            //var reversed = "";
+
+            //for (int i = word.Length -1; i >= 0; i--)
+            //{
+            //   reversed += word[i]; 
+            //}
+
+            //if (reversed.ToLower() == word.ToLower())
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return word.SequenceEqual(word.Reverse());
+
+
+        
+       }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+                
+         [Theory]
+         [InlineData("racecar", true)]
+         [InlineData("hello", false)]
+         [InlineData("Racecar", false)]
 
-        }
+        public void MyTest(string word, bool expected)
+         {
+             //arrange
+             var tester = new Palindrome();
+
+             //act
+             bool actual = tester.IsAPalidrome(word);
+
+             //assert
+             Assert.Equal(expected, actual);
+         }
+
     }
 }
